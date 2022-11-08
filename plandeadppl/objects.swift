b6017bc72d7f90bb362d92fb8efe10dec8eecc51
howard <howardwan_8887@yahoo.com>
2022-11-08 16:02:21 +0800

    color tone

--- plandeadppl/objects.swift
+++ plandeadppl/objects.swift
@@ -9,4 +9,13 @@ import Foundation
 import UIKit
 
 // Color Tones
-let MainColor = UIColor(red: 13/255, green: 47/255, blue: 48/255, alpha: 1)
+let ColorMain1 = UIColor(red: 13/255, green: 47/255, blue: 48/255, alpha: 1)        //dark green
+let ColorMain1a = UIColor(red: 13/255, green: 47/255, blue: 48/255, alpha: 0.9)     //dark green transparent
+let ColorMain2 = UIColor(red: 0/255, green: 153/255, blue: 151/255, alpha: 1)       //tiffany blue
+let ColorMain3 = UIColor(red: 118/255, green: 194/255, blue: 166/255, alpha: 1)     //light green
+let ColorMain4 = UIColor(red: 207/255, green: 225/255, blue: 225/255, alpha: 1)     //pale green
+
+let ColorAux1 = UIColor(red: 1, green: 1, blue: 1, alpha: 1)                        //white
+let ColorAux2 = UIColor(red: 0, green: 0, blue: 0, alpha: 1)                        //black
+let ColorAux3 = UIColor(red: 1, green: 0, blue: 0, alpha: 1)                        //red
+
