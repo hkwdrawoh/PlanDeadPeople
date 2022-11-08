339840d53c5c843df25c934c31f2f3aae799d8e4
howard <howardwan_8887@yahoo.com>
2022-11-08 17:19:00 +0800

    core model data update

--- plandeadppl/Persistence.swift
+++ plandeadppl/Persistence.swift
@@ -13,9 +13,13 @@ struct PersistenceController {
     static var preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+        for index in 0..<10 {
+            let newCourse = Course(context: viewContext)
+            newCourse.timestamp = Date()
+            newCourse.cid = UUID()
+            newCourse.csub = "ELEC"
+            newCourse.cnum = "3644"
+            newCourse.title = "Mobile App Development"
         }
         do {
             try viewContext.save()
