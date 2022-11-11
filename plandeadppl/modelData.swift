a64ea68cd46a19a14c22b00982b0b7e8be3e0a69
howard <howardwan_8887@yahoo.com>
2022-11-11 17:00:29 +0800

    calendar view draft

--- plandeadppl/modelData.swift
+++ plandeadppl/modelData.swift
@@ -9,7 +9,7 @@ import Foundation
 import CoreData
 
 func importCourse(_ viewContext: NSManagedObjectContext) {
-    for index in 1...20 {
+    for index in 1...40 {
         let newCourse = Course(context: viewContext)
         newCourse.id = UUID()
         newCourse.cid = Int16(index)
@@ -21,13 +21,13 @@ func importCourse(_ viewContext: NSManagedObjectContext) {
 }
 
 func importUser(_ viewContext: NSManagedObjectContext) {
-    for index in 1...20 {
-        let newCourse = Course(context: viewContext)
-        newCourse.id = UUID()
-        newCourse.cid = Int16(index)
-        newCourse.csub = "ELEC"
-        newCourse.cnum = String(Int.random(in: 1001...4999))
-        newCourse.title = "Mobile Application Development"
-        newCourse.sem = ["1", "2", "S"].randomElement()
+    for index in 1..<2 {
+        let newUser = User(context: viewContext)
+        newUser.id = UUID()
+        newUser.uid = "guest"
+        newUser.uname = "Guest"
+        newUser.timetablesem1 = [1, 9]
+        newUser.timetablesem2 = [4, 7]
+        newUser.timetablesem3 = [8, 2]
     }
 }
