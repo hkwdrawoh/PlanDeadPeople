3b4e1f2330c8bca244df201ee84dfbd31e838e0f
hihi <antheacc01@gmail.com>
2022-11-18 23:41:15 +0800

    Finish course description map redirection

--- plandeadppl/DescriptionPage.swift
+++ plandeadppl/DescriptionPage.swift
@@ -7,11 +7,34 @@
 
 import SwiftUI
 import CoreData
+import MapKit
+import Contacts
 
 struct CourseDescription: View {
     
     @Binding var menu: String
     @Binding var course: Course
+    
+    @EnvironmentObject var locationManager: LocationManager
+    
+    @State var locationName=""
+    @State var showAlert = false
+    @State var errorTitle=""
+    @State var errorMessage=""
+    @State var result=""
+    @State var mapItem:MKMapItem?
+    @State var placemark:CLPlacemark?
+    
+    enum places: CaseIterable {
+        case LE, MWT
+        
+        var rawValue: (Float64, Float64) {
+            switch self {
+                case .LE: return (22.283309, 114.137784)
+                case .MWT: return (22.282276, 114.139125)
+            }
+        }
+    }
     
     var body: some View {
         VStack (spacing: 0) {
@@ -66,7 +89,24 @@ struct CourseDescription: View {
                         .foregroundColor(ColorAux4)
                         .multilineTextAlignment(.leading)
                         .padding([.top, .leading, .trailing], 10)
-                    Button{} label: {
+                    Button{
+                        for place in places.allCases {
+                            let value = place
+                            let str = String(describing: value)
+                            if str == course.loc {
+                                var lat = value.rawValue.0
+                                var long = value.rawValue.1
+                                locationName = "\(lat),\(long)"
+                                let url = URL(string: "http://maps.apple.com/?daddr=\(lat),\(long)")
+                                if UIApplication.shared.canOpenURL(url!) {
+                                    UIApplication.shared.open(url!)
+                                }
+                            }
+                            if let m = mapItem {
+                                m.openInMaps()
+                            }
+                        }
+                    } label: {
                         Image(systemName: "map")
                             .resizable(resizingMode: .stretch)
                             .frame(width: 22, height: 22)
@@ -77,6 +117,7 @@ struct CourseDescription: View {
                     .background(ColorMain2)
                     .cornerRadius(10)
                     Spacer()}
+                Text(locationName)
                 Text("Description: "+course.desc)
                     .font(.system(size: 20))
                     .foregroundColor(ColorAux4)
@@ -85,6 +126,23 @@ struct CourseDescription: View {
                     
             }
             .background(ColorMain4)
+        }
+    }
+    
+    func updateAddress(placemarks:[CLPlacemark]?,error:Error?) {
+        mapItem = nil
+        if error != nil {
+            errorTitle = "Error"
+            errorMessage = "Geo failed with error \(error!.localizedDescription)"
+            showAlert = true
+        }
+        else if let marks = placemarks, marks.count > 0 {
+            placemark = marks[0] as CLPlacemark
+            if let address: CNPostalAddress = placemark!.postalAddress {
+                let place = MKPlacemark (coordinate: placemark!.location!.coordinate, postalAddress: address)
+                mapItem = MKMapItem(placemark: place)
+                mapItem?.name = locationName
+            }
         }
     }
 }
