0f664361b7282355ab7470697c83c2ee33ffeaa8
howard <howardwan_8887@yahoo.com>
2022-11-21 01:23:56 +0800

    removed navigation view

--- plandeadppl/CoursePage.swift
+++ plandeadppl/CoursePage.swift
@@ -28,119 +28,116 @@ struct CourseList: View {
     var body: some View {
         let user = users[users.firstIndex(where: {$0.uid == uid})!]
         let courses = filterCourse(user, coursesData, sort_true, filter_true)
-        NavigationView {
-            VStack (spacing: 0) {
-                HStack {
-                    //Button - Cancel<->Back Switching Mechanism
-                    Button{
-                        if showSheet {
-                            sort_proposed = sort_true
-                            filter_proposed = filter_true
-                            showSheet.toggle()
-                        } else {
-                            menu = menuselect[0]
-                        }
-                    } label: {
-                        if showSheet {
-                            Text("Cancel")
-                                .foregroundColor(ColorAux1)
-                                .font(.system(size: 18))
-                                .bold()
-                        } else {
-                            Image(systemName: "arrow.uturn.backward")
-                                .resizable(resizingMode: .stretch)
-                                .frame(width: 22, height: 22)
-                                .foregroundColor(ColorAux1)
-                        }
-                    }
-                        .padding(.horizontal, 12)
-                        .padding(.vertical, 12)
-                        .background(ColorMain3)
-                        .cornerRadius(10)
-                    Spacer()
-                    //Button - Filter<->Apply Switching Mechanism
-                    Button{
-                        if showSheet {
-                            sort_true = sort_proposed
-                            filter_true = filter_proposed
-                        }
+        VStack (spacing: 0) {
+            HStack {
+                //Button - Cancel<->Back Switching Mechanism
+                Button{
+                    if showSheet {
+                        sort_proposed = sort_true
+                        filter_proposed = filter_true
                         showSheet.toggle()
-                    } label: {
-                        if showSheet {
-                            Text("Apply")
-                                .foregroundColor(ColorAux1)
-                                .font(.system(size: 18))
-                                .bold()
-                        } else {
-                            Image(systemName: "list.bullet.indent")
-                                .resizable(resizingMode: .stretch)
-                                .frame(width: 22, height: 22)
-                                .foregroundColor(ColorAux1)
-                        }
+                    } else {
+                        menu = menuselect[0]
                     }
+                } label: {
+                    if showSheet {
+                        Text("Cancel")
+                            .foregroundColor(ColorAux1)
+                            .font(.system(size: 18))
+                            .bold()
+                    } else {
+                        Image(systemName: "arrow.uturn.backward")
+                            .resizable(resizingMode: .stretch)
+                            .frame(width: 22, height: 22)
+                            .foregroundColor(ColorAux1)
+                    }
+                }
                     .padding(.horizontal, 12)
                     .padding(.vertical, 12)
                     .background(ColorMain3)
                     .cornerRadius(10)
+                Spacer()
+                //Button - Filter<->Apply Switching Mechanism
+                Button{
+                    if showSheet {
+                        sort_true = sort_proposed
+                        filter_true = filter_proposed
+                    }
+                    showSheet.toggle()
+                } label: {
+                    if showSheet {
+                        Text("Apply")
+                            .foregroundColor(ColorAux1)
+                            .font(.system(size: 18))
+                            .bold()
+                    } else {
+                        Image(systemName: "list.bullet.indent")
+                            .resizable(resizingMode: .stretch)
+                            .frame(width: 22, height: 22)
+                            .foregroundColor(ColorAux1)
+                    }
                 }
-                .padding(.horizontal)
+                .padding(.horizontal, 12)
+                .padding(.vertical, 12)
+                .background(ColorMain3)
+                .cornerRadius(10)
+            }
+            .padding(.horizontal)
+            
+            
+            
+            if showSheet {
+                // Title
+                Text("Course List - Filter")
+                    .font(.largeTitle)
+                    .foregroundColor(ColorAux4)
+                    .bold()
+                    .multilineTextAlignment(.center)
                 
+                //Call Filter Config view
+                ListFilter(sort: $sort_proposed, filter: $filter_proposed)
+            } else {
+                // Title
+                Text(filter_true != filter_default ? "Course List - Filtered" : "Course List")
+                    .font(.largeTitle)
+                    .foregroundColor(ColorAux4)
+                    .bold()
+                    .padding(.bottom)
                 
-                
-                if showSheet {
-                    // Title
-                    Text("Course List - Filter")
-                        .font(.largeTitle)
-                        .foregroundColor(ColorAux4)
-                        .bold()
-                        .multilineTextAlignment(.center)
+                //Show back CourseList default view
+                VStack {
+                    if !filter_true[0] {
+                        SemSelect(sem: $sem)    //HeaderButtons
+                            .padding(.top, -10)
+                    }
                     
-                    //Call Filter Config view
-                    ListFilter(sort: $sort_proposed, filter: $filter_proposed)
-                } else {
-                    // Title
-                    Text(filter_true != filter_default ? "Course List - Filtered" : "Course List")
-                        .font(.largeTitle)
-                        .foregroundColor(ColorAux4)
-                        .bold()
-                        .padding(.bottom)
-                    
-                    //Show back CourseList default view
-                    VStack {
-                        if !filter_true[0] {
-                            SemSelect(sem: $sem)    //HeaderButtons
-                                .padding(.top, -10)
-                        }
-                        
-                        ScrollView {
-                            if courses.filter({return $0.sem == sem}).count == 0 {
-                                Rectangle()
-                                    .frame(height: 1)
-                                Text("No courses found!")
-                                    .font(.title2)
-                            } else {
-                                ForEach(courses) { course in
-                                    if course.sem == sem {
-                                        Rectangle()
-                                            .frame(height: 1)
-                                        Button {
-                                            menu = menuselect[3]
-                                            course_desc = course
-                                        } label: {
-                                            CourseRow(course: course)
-                                        }
+                    ScrollView {
+                        if courses.filter({return $0.sem == sem}).count == 0 {
+                            Rectangle()
+                                .frame(height: 1)
+                            Text("No courses found!")
+                                .font(.title2)
+                        } else {
+                            ForEach(courses) { course in
+                                if course.sem == sem {
+                                    Rectangle()
+                                        .frame(height: 1)
+                                    Button {
+                                        menu = menuselect[3]
+                                        course_desc = course
+                                    } label: {
+                                        CourseRow(course: course)
                                     }
                                 }
                             }
                         }
-                        .foregroundColor(ColorAux4)
-                        .background(ColorMain4)
                     }
+                    .foregroundColor(ColorAux4)
+                    .background(ColorMain4)
                 }
             }
-            .background(ColorMain4)
-            .navigationBarHidden(true)
         }
+        .background(ColorMain4)
     }
 
 }
